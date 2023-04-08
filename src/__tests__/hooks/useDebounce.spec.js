3125b20b521d8ff0605a0fd648f9e2f4b6f79e39
Emanuel Castillo <emanuelcastillo9101@gmail.com>
2023-04-08 18:08:51 -0400

    ready testing

--- src/__tests__/hooks/useDebounce.spec.js
+++ src/__tests__/hooks/useDebounce.spec.js
@@ -1,18 +1,19 @@
+/* eslint-env jest */
 import { renderHook, waitFor } from '@testing-library/react'
 import { useDebounce } from '../../hooks/useDebounce'
 
 describe('useDebounce', () => {
   it('should return the debounced value after the specified time', async () => {
     const { result } = renderHook(() => useDebounce('test', 500))
-		await waitFor(() => {
-			expect(result.current).toBe('test')
-		});
-		await waitFor(() => {
-			expect(result.current).toBe('test')
-		});
+    await waitFor(() => {
+      expect(result.current).toBe('test')
+    })
+    await waitFor(() => {
+      expect(result.current).toBe('test')
+    })
   })
 
-	it('should update the debounced value when the value changes', async () => {
+  it('should update the debounced value when the value changes', async () => {
     const { result } = renderHook(() => useDebounce('test', 500))
     expect(result.current).toBe('test')
     expect(result.current).toBe('test')
